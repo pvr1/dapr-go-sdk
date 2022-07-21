3d526901d63cffe81fdfcbf440c8fde6db4e6f06
Per von Rosen <pvonrosen@gmail.com>
2022-07-21 10:32:08 +0200

    changed context timeout to 5 secs from 1 sec to avoid timeout error
    Signed-off-by: Per von Rosen <pvonrosen@gmail.com>

--- client/client.go
+++ client/client.go
@@ -216,7 +216,7 @@ func NewClientWithAddress(address string) (client Client, err error) {
 	}
 	logger.Printf("dapr client initializing for: %s", address)
 
-	ctx, ctxCancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, ctxCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	conn, err := grpc.DialContext(
 		ctx,
 		address,
